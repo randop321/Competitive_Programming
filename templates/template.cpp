59d806d9509d263d7b82ba267b94f70fc4a3b86c
randop <randop321@outlook.com>
2022-01-16 17:15:44 +0800

    cpp temp

---
+++ templates/template.cpp
@@ -0,0 +1,29 @@
+#include<map>
+#include<set>
+#include<cstdio>
+#include<cstring>
+#include<cmath>
+#include<vector>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+typedef pair<int,int> pii;
+typedef map<int,int> mii;
+typedef set<int> si;
+typedef vector<int> vi;
+typedef long long ll;
+typedef long double ld;
+#define pb push_back
+#define mp make_pair
+/*
+primes for hash 
+const int base[2] = {307,521};
+const int mod[2] = {1000000007, 900000011};
+*/
+
+
+int main()
+{
+	return 0;
+}
